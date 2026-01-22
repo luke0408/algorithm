8b5730007df404abe55dff86bbc507f3da5dc86e
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2026-01-23 03:03:11 +0900

    Title: 왔다 갔던 구역 2, Time: 72ms, Memory: 11MB, Status: Passed - Codetree

--- 260123/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 260123/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -6,6 +6,7 @@ const commands = input.slice(1, n + 1);
 
 // Please Write your code here.
 const ary = Array(2001).fill(0);
+const target = commands.map((c) => c.split(' '));
 let x = 1001
 
 const moving = (ary, cnt, move) => {
@@ -33,8 +34,9 @@ const moving = (ary, cnt, move) => {
 
 const result = () => {
     let tmp = ary;
-    for (c of commands) { 
-        tmp = moving(tmp, Number(c[0]), c[2]); 
+    for (c of target) { 
+        tmp = moving(tmp, Number(c[0]), c[1]); 
+        // console.log(c[0], c[1], x);
     }
     return tmp.filter((v) => v >= 2).length;
 }
